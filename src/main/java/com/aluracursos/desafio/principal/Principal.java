6930beb79e666dd441b4126a6cde9270b0d184d9
Omar García López <omar.garcia@omargl.net>
2024-05-13 12:07:48 -0600

    top 10 downloaded books

---
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -0,0 +1,33 @@
+package com.aluracursos.desafio.principal;
+
+import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
+import com.aluracursos.desafio.service.ConsumoAPI;
+import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
+
+public class Principal {
+
+    private static final String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu(){
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
+
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+        // Top 10 libros mas descargados
+        System.out.println("Top 10 Descargados");
+
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+    }
+}
